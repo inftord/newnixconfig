25f33a6a491497559728f760540efaca734a6684
InFTord <inftord@gmail.com>
2026-01-09 01:13:57 +0500

    cachyos kernel :3

--- flake.nix
+++ flake.nix
@@ -5,16 +5,27 @@
     # NixOS official package source, using the nixos-25.11 branch here
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.acertop = nixpkgs.lib.nixosSystem {
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./hosts/acertop/configuration.nix
-      ];
-    };
+	    modules = [
+	      (
+	        { pkgs, ... }:
+	        {
+	          nixpkgs.overlays = [
+	            # Alternatively: use the exact kernel versions as defined in this repo.
+	            # Guarantees you have binary cache.
+	            self.overlays.pinned
+
+	            # Only use one of the two overlays!
+	          ];
+		  ./hosts/acertop/configuration.nix
+	        }
+	      )
+	    ];
+	};
   };
 }
